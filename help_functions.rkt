799fc131d694bda1937bfca901d23664c18de07f
shenxs <a247820400@gmail.com>
2016-06-14 19:47:24 +0800

    精简

--- help_functions.rkt
+++ help_functions.rkt
@@ -5,6 +5,8 @@
 ;;尽量不涉及图片数据
 ;;鸡蛋不能放在一个篮子里面
 ;;captian my captian
+
+
 (define (color->n p x y )
   (let([P (get-pixel-color x y p)])
     (cond
@@ -23,33 +25,24 @@
     [else (cons a (to (+ a 1) b ))]))
 
 (define (img->list/n p x0 y0 x y)
-  ;(display x) (display " ") (displayln y)
   (for*/list ([i ( x0 . to . x)]
               [j ( y0 . to . y)])
     (color->n p i j)))
 
-
 (define (img->list p x0 y0 x y)
-  ;(display x) (display " ") (displayln y)
   (for*/list ([i ( x0 . to . x)]
               [j ( y0 . to . y)])
     (get-pixel-color  i j p)))
 
 (define (touch? p1 p2 s-x s-y e-x e-y)
-  ;(display p1) (displayln "\n") (display p2) (display s-x)
   (local ((define p1-list (img->list/n p1 s-x s-y e-x e-y))
           (define p2-list (img->list/n p2 s-x s-y e-x e-y))
           (define (foo l1 l2)
-            ;(display (first l1)) (displayln (first l2))
             (cond
               [(and (empty? l1) (empty? l2)) #f]
-              ;[(= (first l1) (first l2)) (display "一样")(foo (rest l1) (rest l2))]
               [(and (= 1 (first l1))
                     (= 3 (first l2))) #t]
-              [else (foo (rest l1) (rest l2))]))
-          )
-    ;(displayln p1-list)
-    ;(displayln p2-list)
+              [else (foo (rest l1) (rest l2))])))
     (foo p1-list p2-list)))
 
 (provide (all-defined-out))
